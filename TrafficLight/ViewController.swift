f3b8a1fa0a135399c6290dfe9de460134fff3925
aleksei <alikexie93@yandex.ru>
2023-03-20 23:47:00 +0600

    Bug fix

--- TrafficLight/ViewController.swift
+++ TrafficLight/ViewController.swift
@@ -7,7 +7,7 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+final class ViewController: UIViewController {
     
     @IBOutlet var redTrafficLight: UIView!
     @IBOutlet var yellowTrafficLight: UIView!
@@ -15,48 +15,51 @@ class ViewController: UIViewController {
     
     @IBOutlet var startButton: UIButton!
     
-    private var currentLight = ""
+    private var currentLight = CurrentLight.red
+    //Здесь мы задали цвета через перечисление enum и используем их через расширение extension, и задали текущий цвет красный
     private let lightOn: CGFloat = 1.0
     private let lightOff: CGFloat = 0.3
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         startButton.layer.cornerRadius = 16
         
-        redTrafficLight.layer.cornerRadius = 50
-        yellowTrafficLight.layer.cornerRadius = 50
-        greenTrafficLight.layer.cornerRadius = 50
-        
         redTrafficLight.alpha = lightOff
         yellowTrafficLight.alpha = lightOff
         greenTrafficLight.alpha = lightOff
+        
+        redTrafficLight.layer.cornerRadius = redTrafficLight.frame.height / 2
+        yellowTrafficLight.layer.cornerRadius = yellowTrafficLight.frame.height / 2
+        greenTrafficLight.layer.cornerRadius = greenTrafficLight.frame.height / 2
     }
-
+    
     @IBAction func startButtonTapped() {
         if startButton.currentTitle == "Start" {
             startButton.setTitle("Next", for: .normal)
-            currentLight = "red"
+            //currentLight = .red
+            //redTrafficLight.alpha = lightOn
+            //Ранее мы уже установили текущий свет и поэтому не нужно его устанавливать в данном методе
+        }
+        switch currentLight {
+        case .red:
+            greenTrafficLight.alpha = lightOff
             redTrafficLight.alpha = lightOn
-        } else {
-            switch currentLight {
-            case "red":
-                redTrafficLight.alpha = lightOff
-                yellowTrafficLight.alpha = lightOn
-                currentLight = "yellow"
-            case "yellow":
-                yellowTrafficLight.alpha = lightOff
-                greenTrafficLight.alpha = lightOn
-                currentLight = "green"
-            case "green":
-                greenTrafficLight.alpha = lightOff
-                redTrafficLight.alpha = lightOn
-                currentLight = "red"
-            default:
-                break
-            }
+            currentLight = .yellow
+        case .yellow:
+            redTrafficLight.alpha = lightOff
+            yellowTrafficLight.alpha = lightOn
+            currentLight = .green
+        case .green:
+            greenTrafficLight.alpha = lightOn
+            yellowTrafficLight.alpha = lightOff
+            currentLight = .red
         }
     }
     
-
 }
 
+extension ViewController {
+    private enum CurrentLight {
+        case red, yellow, green
+    }
+}
