3f5e923485f9c6217a34debcd218f3b6b6c9a572
aleksei <alikexie93@yandex.ru>
2023-03-20 23:04:45 +0600

    Deleted comments

--- TrafficLight/ViewController.swift
+++ TrafficLight/ViewController.swift
@@ -21,39 +21,32 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        //Let corner radius to button
         startButton.layer.cornerRadius = 16
         
-        //Change traffic lights to circle
         redTrafficLight.layer.cornerRadius = 50
         yellowTrafficLight.layer.cornerRadius = 50
         greenTrafficLight.layer.cornerRadius = 50
         
-        //Turn lights off
         redTrafficLight.alpha = lightOff
         yellowTrafficLight.alpha = lightOff
         greenTrafficLight.alpha = lightOff
     }
 
     @IBAction func startButtonTapped() {
-        //Change button title to "Next" while press and red light ON
         if startButton.currentTitle == "Start" {
             startButton.setTitle("Next", for: .normal)
             currentLight = "red"
             redTrafficLight.alpha = lightOn
         } else {
             switch currentLight {
-            //Switch light color to yellow if current light is red
             case "red":
                 redTrafficLight.alpha = lightOff
                 yellowTrafficLight.alpha = lightOn
                 currentLight = "yellow"
-            //Switch light color to green if current light is yellow
             case "yellow":
                 yellowTrafficLight.alpha = lightOff
                 greenTrafficLight.alpha = lightOn
                 currentLight = "green"
-            //Switch light color to red if current light is green
             case "green":
                 greenTrafficLight.alpha = lightOff
                 redTrafficLight.alpha = lightOn
